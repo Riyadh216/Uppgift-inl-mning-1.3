25a1fd0664d9ccd18780606b7cfe645b4b39c13e
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:20:09 +0200

    Add project files.

---
+++ Uppgift inlämning 1.3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    / \\");
+            Console.WriteLine("   /   \\");
+            Console.WriteLine("  /     \\   ");
+            Console.WriteLine(" / ______\\");
+            Console.ReadKey();
+        }
+    }
+}
